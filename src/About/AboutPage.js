59a607548cd40f65a9f5cfef8ef13d1ee0353ae6
koo <nicewdjql@naver.com>
2025-01-18 01:58:41 +0900

    01-17:20-03

--- src/About/AboutPage.js
+++ src/About/AboutPage.js
@@ -116,11 +116,13 @@ function App() {
               <  td>2024.06 ~ 2024.12</td>
               <td>
                 <ul className='table-project-item '>
-                  <li>리눅스: 웹관리, 데이터관리, 도커를 통한 컨테이너 관리</li>
+                  <li>Node.js: 서버구축 및 token 및 key값 관리, 트래픽관리</li>
+                  <li>리엑트: 프론트엔드 구축과 부트스트랩, 동적환경 구축</li>
+                  <li>리눅스: NGINX, Docker를 통한 컨테이너 관리, 로드밸런싱</li>
                   <li>JAVA: JAVA 코드 활용하여 클라이언트 / 서버 구축</li>
                   <li>JSP: JSP를 통한 백엔드 구축</li>
-                  <li>SQL: 데이터베이스 숙달</li>
-                  <li>Python: FastAPI를 통해 HTML과 클라이언트 / 서버 구축</li>
+                  <li>SQL: MySQL, MongoDB</li>
+                  <li>AWS: 3tier 구축 및 서비스배포, 로드밸런싱</li>
                 </ul>
               </td>
             </tr>
@@ -136,22 +138,37 @@ function App() {
       <section className="about-experience">
         <h2>□ 경력</h2>
         <div className="about-experience-item">
-          <h3>▹육군본부</h3>
+          <h3>○ 육군 장교복부(ROTC#53)</h3>
           <ul className='about-project-item'>
-          <li>직책: 그룹장</li>
-          <li>기간: 2015.03 ~ 2024.07 (총 9년 4개월)</li>
-          <li>연봉: 3,800만원</li>
+          <li>직책: 지휘관, 참모과장</li>
+          <li>병과: 보병</li>
+          <li>기간: 2015.03 ~ 2024.06 (총 9년 4개월)</li>
           </ul>
 
-          <h4 className='subtitle'>▹주요 업무 :</h4>
+          <h4 className='subtitle'>○ 주요 업무 :</h4>
           <ul className='about-project-item'>
-            <li>리더, 과장 직책을 통한 조직 관리 및 다양한 업무 경험</li>
-            <li>기획, 재정관리, 군수관리 등의 업무 수행</li>
+            <li>지휘관으로 조직관리 및 계획수립</li>
+            <li>참모과장으로 기획, 재정관리, 군수관리 등의 업무 수행</li>
           </ul>
-          <h4  className='subtitle'>▹주요 성과:</h4>
+          <h4  className='subtitle'>○ 주요 성과:</h4>
           <ul className='about-project-item'>
-            <li>조직관의 관계 관리</li>
-            <li>다양한 업무 수행을 통한 문제 해결 능력 향상</li>
+            <li>16개 중대 중 1등 선봉중대 달성</li>
+            <li>여단급 명령하달 경연대회 1등 수상</li>
+            <li>각종 감사 우수 유공(보안,재정,동원감사)</li>
+            <li>OBC 교육과정 우수 교육사령관 표창 (백분률 상위 18%) </li>
+            <li>개인평가 최우수 특급전사 달성 </li>
+            <li>안정적이고 능동적인 조직관리</li>
+            <li>다양한 참모경험으로 조직기여</li>
+          </ul>
+          <h4  className='subtitle'>○ 기여할 수 있는 점:</h4>
+          <ul className='about-project-item'>
+            <li>새로운 일, 다양한 일에 대해서 극복하고 적응할 수 있는 힘이 있습니다.</li>
+            <li>매순간 최선을 다하고 성과를 이뤄내는 경험이 있습니다.</li>
+            <li>다양한 신분, 연령이 있는 조직에서 일을 통해 팀업무 수행에 자신있습니다.</li>
+            <li>상급자의 요구사항을 잘 파악하여 사전에 준비하고 해결해낼 수 있습니다. </li>
+            <li>위 경험을 통해 내실 있는 신입 개발자가 되도록 하겠습니다.  </li>
+ 
+
           </ul>
         </div>
         <main className="chargeinfo-page">
@@ -177,9 +194,13 @@ function App() {
           <td>프론트엔드 개발 - 사용자 인터페이스 최적화</td>
           <td>
             <ul className='about-project-item'>
-              <li><strong>React:</strong> 다소스튜디오 카페의 메인 UI 개발에 주로 사용. 컴포넌트 기반 구조로 개발하여 유연성과 확장성을 유지하며 코드 재사용성을 높입니다. 상태 관리 및 라이프사이클 메서드를 활용하여 사용자 경험을 동적으로 제공합니다.</li>
-              <li><strong>React Router:</strong> 페이지 간 원활한 이동과 라우팅을 지원하여 사용자가 필요한 정보를 쉽게 찾을 수 있게 해줍니다.</li>
-              <li><strong>React Hooks:</strong> useState, useEffect와 같은 Hooks를 활용하여 컴포넌트의 상태 및 사이드 이펙트를 효율적으로 관리하고 코드 가독성과 유지보수성을 향상시킵니다.</li>
+              <li><strong>오픈소스 활용 : </strong>네이버로그인. 카카오맵, coolsms(SNS인증) </li>
+              <li><strong>웹 구성 : </strong>메인페이지, 헤더, 사이드바, 로그인, 회원가입, 게시판, 방명록, JWT Token, key값 호출</li>
+              <li><strong>동적 기능 : </strong>React Router, Pagination, navigate, 배열, Hooks, Axios, Event Handling, 토스트메세지</li>
+              <li><strong>token 디코딩 : </strong>서버에 토큰을 요청하고 디코딩하여 정보를 추출함</li>
+              <li><strong>사용자 관리 : </strong>추출한 토큰정보로 게시글 관리</li>
+              <li><strong>Redux  : </strong>dispatch(getOrder())를 통해 주문 데이터를 가져오는 요청이 Redux 스토어로 전달</li>
+
             </ul>
           </td>
         </tr>
@@ -188,20 +209,23 @@ function App() {
           <td>백엔드 서버 - 서버 및 API 기반 구축</td>
           <td>
             <ul className='about-project-item'>
-              <li><strong>Node.js:</strong> Express.js와 함께 RESTful API 설계에 사용되며, 비동기 처리를 통해 데이터 처리 효율성을 높입니다.</li>
-              <li><strong>JWT:</strong> JSON Web Token을 통해 사용자 인증 및 세션 관리를 강화해 보안성을 높입니다.</li>
-              <li><strong>Socket.io:</strong> 실시간 채팅 기능을 구현하여 사용자 간의 실시간 소통이 가능하도록 지원합니다.</li>
+              <li><strong>bcrypt : </strong> 비밀번호 Hash로 변경하여 저장 </li>
+              <li><strong>JWT : </strong>인증, 인가를 통한 로그인, 게시판작성, 방명록 작성 관리(auth.controller.js)</li>
+              <li><strong>TINY_MCE_API : </strong>게시판 라이브러리, API KEY 값 관리</li>
+              <li><strong>회원가입 / 로그인 : </strong>유효성검사, 중복체크, token 저장</li>
+              <li><strong>방명록 : </strong>token값에 따른 CURD 관리</li>
+              <li><strong>캐싱 : </strong>sessionStorage와 localStorage 저장방식 선택</li>
             </ul>
           </td>
         </tr>
         <tr>
-          <td>MySQL</td>
+          <td>MySQL / MongoDB</td>
           <td>데이터베이스 관리 - 데이터베이스 설계 및 성능 최적화</td>
           <td>
             <ul className='about-project-item'>
-              <li><strong>MySQL:</strong> 관계형 데이터베이스로, 데이터 구조 설계 및 관리에 최적화되어 있습니다.</li>
-              <li><strong>SQL 쿼리 최적화:</strong> 대량 데이터를 효율적으로 처리하기 위해 인덱스를 사용하여 검색 성능을 크게 향상시킵니다.</li>
-              <li><strong>ORM 사용 (Sequelize):</strong> ORM을 통해 데이터베이스와의 상호작용을 직관적으로 처리하며 코드 유지보수성을 높입니다.</li>
+              <li><strong>정규화 : </strong> 안정적 데이터관리, 데이터 업데이트를 위한 정규화를 위해 조별 토의와 의견수렴</li>
+              <li><strong>ERD : </strong> 독립적인 데이터가 생기지 않도록 관계형데이터 구현하도록 설계</li>
+              <li><strong>ORM 사용 (Sequelize):</strong> 데이터베이스와 상호작용을 직관적으로 처리하며 코드 유지보수성을 높였음.</li>
             </ul>
           </td>
         </tr>
@@ -210,8 +234,11 @@ function App() {
           <td>서버 운영 관리 - 서버 환경 설정 및 관리 효율성 증대</td>
           <td>
             <ul className='about-project-item'> 
-              <li><strong>Linux:</strong> 서버 배포 및 유지관리에 리눅스를 활용하여 파일 시스템 구조를 이해하고 서버 성능을 최대화합니다.</li>
-              <li><strong>Shell Script:</strong> 자동화 작업을 위해 Bash 스크립트를 활용, 반복 작업의 효율성을 증가시킵니다.</li>
+              <li><strong>Telnet : </strong> xinetd를 통한 텔넷 원격운용</li>
+              <li><strong>ssh : </strong> ssh_config, sshd_config를 통한 ssh설정과 mobaxterm연결을 해봤습니다.</li>
+              <li><strong>RAID : </strong> 디스크 로드밸런싱을 위한 디스크 설정을 해보았습니다.</li>
+              <li><strong>LVM : </strong> 손쉬운 디스크 용량확장을 위해 설정했으며 VG에 증설하였습니다.</li>
+              <li><strong>DNS: </strong> bind와 ZONE을 구성하여 DNS설정하여 가비아 도메인과 연결하였습니다.</li>
             </ul>
           </td>
         </tr>
@@ -220,8 +247,10 @@ function App() {
           <td>컨테이너화 및 배포 - 컨테이너화 및 서비스 배포 일원화</td>
           <td>
             <ul className='about-project-item'>
-              <li><strong>Docker:</strong> 서비스 환경의 일관성을 유지하며, 다양한 환경에서도 동일한 성능을 보장합니다.</li>
-              <li><strong>Docker Compose:</strong> 여러 서비스 간의 의존성을 관리하여 동시에 설정하고 관리할 수 있습니다.</li>
+              <li><strong>Docker Nginx : </strong> Nginx를 활용한 컨테이너 proxy기능 활용</li>
+              <li><strong>Docker Compose : </strong> Docker compose YAML 코드 작성과 3-Tier 배포 활용</li>
+              <li><strong>Docker image : </strong> 간편한 배포와 유지보수를 위해 docker hub로 배포</li>
+              <li><strong>ECS 구성 : </strong> AmazonECS를 활용한 컨테이너 서비스 배포</li>
             </ul>
           </td>
         </tr>
@@ -230,9 +259,9 @@ function App() {
           <td>클라우드 배포 및 관리 - 확장성 높은 서버 관리 및 서비스 제공</td>
           <td>
             <ul className='about-project-item'>
-              <li><strong>Amazon S3 (정적 웹 호스팅):</strong> S3 버킷을 생성하여 정적 웹 페이지를 호스팅합니다. 퍼블릭 액세스를 설정하고 파일을 업로드하여 웹사이트를 구성합니다.</li>
+              <li><strong>Amazon S3 (정적 웹 호스팅):</strong> 리엑트 웹에 관련된 영상, pdf, 정적요소들을 S3버킷에 저장</li>
               <li><strong>Amazon API Gateway (API 관리):</strong> HTTP API를 생성하여 외부 요청을 처리하며, CloudFront와 연계하여 API 요청을 처리하고 Lambda 함수와 통합하여 서버리스 아키텍처를 제공합니다.</li>
-              <li><strong>Amazon RDS (데이터 관리):</strong> 안정적인 데이터 관리 및 성능 최적화를 통해 데이터를 안전하게 운영합니다. 자동 백업 및 복원 기능을 활용하여 MySQL 데이터베이스를 설정합니다.</li>
+              <li><strong>Amazon RDS (데이터 관리):</strong> MySQL 데이터베이스를 활용하여 RDS 설정</li>
               <li><strong>AWS Lambda (서버리스 기능 구현):</strong> 서버리스 아키텍처를 통해 이벤트 기반으로 비즈니스 로직을 자동으로 처리합니다.</li>
               <li><strong>GitHub Actions (CI/CD):</strong> 소스 코드 변경 시 자동으로 배포되도록 CI/CD 파이프라인을 구축합니다. GitHub에서 자동화된 워크플로우를 통해 코드가 푸시될 때마다 배포합니다.</li>
               <li><strong>NGINX (로드밸런싱 및 웹 관리):</strong> 웹 트래픽을 효율적으로 분산 처리하고 로드밸런서를 설정하여 여러 인스턴스에 트래픽을 분산합니다.</li>
@@ -279,16 +308,7 @@ function App() {
         
       </section>
 
-      <footer className="about-footer">
-        <p>위의 모든 기재사항은 사실과 다름없음을 확인합니다.</p>
-        <p>작성자: 구정빈</p>
-        <p>이 이력서는 2024년 09월 24일 (화)에 최종 수정된 이력서 입니다.</p>
-        <p>
-          위조된 문서를 등록하여 취업활동에 이용 시 법적 책임을 지게 될 수 있습니다.
-          잡코리아(유)는 구직자가 등록한 문서에 대해 보증하거나 별도의 책임을 지지 않으며,
-          첨부된 문서를 신뢰하여 발생한 법적 분쟁에 책임을 지지 않습니다.
-        </p>
-      </footer>
+
     </div>
   );
 }
